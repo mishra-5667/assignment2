6a99a954e8035e38c61a1891fa488c77cc12ecfb
USER-HP <USER-HP@SACHIN-HP>
2022-09-28 16:53:08 +0530

    class assignment3

--- corejava/src/corejava/Relational.java
+++ corejava/src/corejava/Relational.java
@@ -4,26 +4,4 @@ import java.util.Scanner;
 
 public class Relational {
 
-	public static void main(String[] args) {
-	//creating scanner class to take input from user
-		Scanner sc=new Scanner(System.in);
-     //getting input value from user
-		System.out.println("enter the first value");
-		int num1=sc.nextInt();
-		System.out.println("enter the second value");
-		int num2=sc.nextInt();
-		System.out.println("enter the third number");
-		int num3=sc.nextInt();
-		//performing equal to operation
-		System.out.println("num1==num2"+(num1==num2));
-		//performing is not equal to operation
-		System.out.println("num1!=num2"+(num1!=num2));
-		//performing greater than operation
-		System.out.println("num1<num2"+(num1<num2));
-		//performing greater than or equal to operation
-		System.out.println("num1<=num2"+(num1<=num2));
-		//performing less than or equal to operation
-		System.out.println("num1>=num2"+(num1>=num2));
-	}
-
 }
